73579d342752abc83a9ea0e9e705140f5b954a54
Mayra Azevedo <mayradazevedo@ufrn.edu.br>
2019-03-13 16:38:20 -0300

    init polygon

---
+++ include/polygon.h
@@ -0,0 +1,29 @@
+#ifndef _polygon_h_
+#define _polygon_h_
+
+#include "graphic.h"
+#include "line.h"
+
+namespace rstzr{
+
+    class Polygon : public Graphic
+    {
+        public:
+            /**
+             * @brief Constructs a polygon based on a json object
+             */
+            Polygon(json &j);
+
+            /**
+             * @brief Default destructor
+             */
+            ~Polygon() = default;
+        private:
+            /* data */
+            std::vector<Line> lines;
+    
+    };
+    
+}
+
+#endif
